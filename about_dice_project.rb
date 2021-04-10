44a1c21ca73c965d7f588c2d00c7f33db9334cd9
Roberto Dip <dip.jesusr@gmail.com>
2021-04-10 13:59:54 -0300

    chore: run rubocop autocorrect

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,7 +6,7 @@ class DiceSet
   attr_reader :values
 
   def roll(n)
-    @values = (1..n).map{|n| rand(1..5)}
+    @values = (1..n).map { |_n| rand(1..5) }
   end
 end
 
@@ -20,7 +20,7 @@ class AboutDiceProject < Neo::Koan
     dice = DiceSet.new
 
     dice.roll(5)
-    assert dice.values.is_a?(Array), "should be an array"
+    assert dice.values.is_a?(Array), 'should be an array'
     assert_equal 5, dice.values.size
     dice.values.each do |value|
       assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
@@ -45,7 +45,7 @@ class AboutDiceProject < Neo::Koan
     second_time = dice.values
 
     assert_not_equal first_time, second_time,
-      "Two rolls should not be equal"
+                     'Two rolls should not be equal'
 
     # THINK ABOUT IT:
     #
@@ -63,5 +63,4 @@ class AboutDiceProject < Neo::Koan
     dice.roll(1)
     assert_equal 1, dice.values.size
   end
-
 end
--- triangle.rb
+++ triangle.rb
@@ -21,11 +21,10 @@ def triangle(a, b, c)
   if a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
-    :isosceles 
+    :isosceles
   else
     :scalene
   end
-
 end
 
 # Error class used in part 2.  No need to change this code.
